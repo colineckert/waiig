c92fdbfcd45a427ed0431d84aaf1b25e6d294132
Colin Eckert <colineckert10@gmail.com>
2024-08-31 10:00:15 -0700

    start lexer tests

---
+++ src/monkey/lexer/lexer_test.go
@@ -0,0 +1,40 @@
+package lexer
+
+import (
+	"testing"
+
+	"monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectType    token.TokenType
+		expectLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectLiteral, tok.Literal)
+		}
+	}
+}
