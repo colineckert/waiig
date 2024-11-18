48c1fa637ee67d104f22f450ec698bd0d2df854d
Colin Eckert <colineckert10@gmail.com>
2024-11-17 20:44:41 -0800

    Add tests for global let statements

--- src/monkey/compiler/compiler.go
+++ src/monkey/compiler/compiler.go
@@ -158,6 +158,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 			}
 		}
 
+	case *ast.LetStatement:
+		err := c.Compile(node.Value)
+		if err != nil {
+			return err
+		}
+
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(integer))
